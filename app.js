8d7d40b8fc9708b5d619586d9d72746c5f1b397c
christiansanchez-SE <christiansanchez714@gmail.com>
2024-06-11 12:29:55 -0400

    adding notes in JS

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+// Still in prduction prompt
+window.alert("Hey Welcome To My Page, Its Still In Production!");
+
 // This is a toggle that turns from bright to dark mode
 const body = document.querySelector("body");
 const btn = document.getElementById("btn");
 
 // btn.classList.toggle("active") toggles the active class on the button when it is clicked 
+// If the button has the active class, the background and text color of the body are set to dark mode colors
+// If the button does not have the active class, the background and text color of the body are set to light mode colors 
+// This has inline JS styling 
 function toggleBtn() {
     btn.classList.toggle("active");
     if (btn.classList.contains("active")) {
@@ -16,10 +22,14 @@ function toggleBtn() {
     }
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // This is the logic behind the Music list
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+// addTask function checks if the input box is empty
+// If its not empty, it creates a new list item ("li") with the song's name and appends it to the list container
+// Each list item also gets a span with an "X" (\u00d7) for removal 
 function addTask(){
     if(inputBox.value === ''){
         alert("Add your song!");
@@ -37,8 +47,10 @@ function addTask(){
     inputBox.value = "";
 }
 
+// Toggles the checked class on the list item if it's clicked, marking it as completed
+// Removes the list item if the "X" span is clicked
 listContainer.addEventListener("click", function(event){
-    if(event.target.tagName === "LI"){
+    if(event.target.tagName === "li"){
         event.target.classList.toggle("checked");
     }
     else if(event.target.tagName === "SPAN"){
@@ -46,17 +58,16 @@ listContainer.addEventListener("click", function(event){
     }
 });
 
-
-/////////////////
-
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Scrolls the page to the top smoothly when called
 function toggleButton(){
     window.scrollTo({top: 0, behavior:'smooth'});
 }
 
-window.alert("Hey Welcome To My Page, Its Still In Production!");
-
-/////////////
-
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// If the input is empty, an error message is displayed and form submission is prevented
+// If the input is not "yes" or "no" (case insensitive), an error message is displayed and form submission is prevented
+// If the input is valid, any existing error message is cleared
 const form = document.getElementById("myForm");
 const chooseInput = document.getElementById("choose");
 const chooseError = document.getElementById("chooseError");
